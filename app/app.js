246380ac07bb21140fafc6693f9e6b936417c54e
Argetloum <florian.mithieux@gmail.com>
2015-01-21 16:35:32 +0100

    Fix proto

--- app/app.js
+++ app/app.js
@@ -2,28 +2,8 @@
 /* global window */
 /* global cordova */
 /* global StatusBar */
-/* global APPLICATION_NAME */
-'use strict'; // jshint ignore:line
 
-// App configuration
-var APPLICATION_NAME = 'ionic-proto'; // jshint ignore:line
-
-// Remember to change the name of the application here, in your "index.html" and in "lang.js"
-angular.module('common', [
-    // Add here any other angular module you need
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'ui.router',
-    'Directives',
-    'Constants',
-    'Factories',
-    'Filters',
-    'Services',
-    'translation',
-    'ionic',
-    'lumx'
-]);
+var APPLICATION_NAME = 'ionic-proto';
 
 // Use theses modules to declare new filters, services, factories and directives
 // so the injector is automatically available
@@ -33,47 +13,65 @@ angular.module('Directives', []);
 angular.module('Factories', ['ngResource']);
 angular.module('Services', []);
 
+angular.module(APPLICATION_NAME,
+    [
+        'ionic',
+        'translation',
+        'lumx',
+        'ngResource',
+        'ngRoute',
+        'Directives',
+        'Constants',
+        'Factories',
+        'Filters',
+        'Services'
+    ])
 
-// Remember to change the name of the application here, in your "index.html" and in "lang.js"
-angular.module(APPLICATION_NAME, [
-    'common'
-])
-    .config(['$stateProvider', '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider)
+    .run(function ($ionicPlatform)
+    {
+        $ionicPlatform.ready(function ()
         {
-            $stateProvider
-                .state('ionic-proto', {
-                    url: '/',
-                    views: {
-                        'content': {
-                            controller: 'HomeCtrl',
-                            templateUrl: '/templates/home/views/home.html'
-                        }
-                    }
-                });
-
-            // if none of the above states are matched, use this as the fallback
-            $urlRouterProvider.otherwise('/');
-        }])
-
-    .run(['$ionicPlatform',
-        function ($ionicPlatform)
-        {
-            $ionicPlatform.ready(function ()
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard)
             {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins.Keyboard)
-                {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                }
-                if (window.StatusBar)
-                {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault();
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar)
+            {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
+
+    .config(function ($stateProvider, $urlRouterProvider)
+    {
+
+        $stateProvider
+
+            // setup an abstract state for the tabs directive
+            .state('ionic-proto', {
+                url: "/app",
+                abstract: true,
+                templateUrl: "templates/common/views/default.html"
+            })
+
+            // Each tab has its own nav history stack:
+            .state('ionic-proto.dash', {
+                url: '/home',
+                views: {
+                    'content': {
+                        templateUrl: 'templates/home/views/home.html',
+                        controller: 'HomeCtrl'
+                    }
                 }
             });
-        }])
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/home');
+
+    })
     .controller('MainCtrl', ['$scope', 'Translation',
         function ($scope, Translation)
         {
